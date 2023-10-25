1c016bb564c614713dd94aafbb3bde9e3e8da076
JPabloLobato <146821982+JPabloLobato@users.noreply.github.com>
2023-10-25 17:04:05 +0200

    Add files via upload

---
+++ ejercicio1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;  int main() {
+    string nombre;
+    int edad;
+    cout << "Por favor ingresa tu nombre: ";
+    cin >> nombre;
+    cout << "Por favor ingresa tu edad: ";
+    cin >> edad;
+    cout << "Hola, " << nombre << "! Tienes " << edad << " anos.\n";
+    return 0;
+}
